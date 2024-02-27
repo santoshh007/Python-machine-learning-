6f8b7bbe1f6dcb353c5b5ba53e9c76c05bd0df30
santosh <santosh1184@xavier.edu.np>
2024-02-27 09:59:19 +0545

    adding task 01 day02

---
+++ Week 02/Day 02/task01.py
@@ -0,0 +1,16 @@
+#task 01
+#Create a simple grading system where a student's score is entered, and the program determines the corresponding grade.
+
+score=float(input("Your Score:"))
+if 90>=100:
+ print("your grade is A")
+
+elif 80>=89:
+ print("your grade is B")
+
+elif 70>=79:
+ print("your grade is C")
+elif 60>=69:
+ print("your grade is D")
+else :
+ print("your grade is F")
